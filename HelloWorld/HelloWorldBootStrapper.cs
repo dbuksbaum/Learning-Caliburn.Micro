1efa730a9e1f35edd8888f13354cfa276b4045d6
David Buksbaum <david@buksbaum.us>
2010-07-31 23:31:28 -0400

    Added Learning.Caliburn.Micro solution. Added HelloWorld project.

---
+++ HelloWorld/HelloWorldBootStrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using HelloWorld.ViewModels;
+
+namespace HelloWorld
+{
+  public class HelloWorldBootStrapper : Bootstrapper<MainViewModel>
+  {
+  }
+}
---
+++ HelloWorld/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace HelloWorld.ViewModels
+{
+  public class MainViewModel : PropertyChangedBase  
+  {
+    private string _name;
+    private string _helloString;
+    
+    public string Name
+    {
+      get { return _name; }
+      set
+      {
+        _name = value;
+        NotifyOfPropertyChange(() => Name);
+        NotifyOfPropertyChange(() => CanSayHello);
+      }
+    }
+    
+    public string HelloString
+    {
+      get { return _helloString; }
+      private set
+      {
+        _helloString = value;
+        NotifyOfPropertyChange(() => HelloString);
+      }
+    }
+
+    public bool CanSayHello
+    {
+      get { return !string.IsNullOrWhiteSpace(Name); }
+    }  
+
+    public void SayHello(string name)
+    {
+      HelloString = string.Format("Hello {0}.", Name);
+    }
+  }
+}
